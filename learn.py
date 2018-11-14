1da3a127608ad273f36f23d824dd10a09def8f72
Jack Stettner <jack@stet.com>
2018-11-14 08:50:39 -0500

    slight change in approach

--- learn.py
+++ learn.py
@@ -15,7 +15,7 @@ import mancala
 
 BRAWLS_PER_GENERATION = 10
 SHOW = False
-epsilon = 1
+epsilon = 0
 
 def eval_genomes(genomes, config):
     """
@@ -27,15 +27,16 @@ def eval_genomes(genomes, config):
 
     for genome_id, genome in genomes:
         genome.fitness = 0
+        pit(genome, genomes[random.randint(0,len(genomes)-1)][1], config)
 
-    for _ in range(BRAWLS_PER_GENERATION):
-        random.shuffle(genomes)
+    # for _ in range(BRAWLS_PER_GENERATION):
+        # random.shuffle(genomes)
 
-        i = 0
+        # i = 0
         # print(len(genomes))
-        while i+1 < len(genomes):
-            pit(genomes[i][1], genomes[i+1][1], config)
-            i += 2
+        # while i+1 < len(genomes):
+        #     pit(genomes[i][1], genomes[i+1][1], config)
+        #     i += 2
 
     # brings all fitness values between 0 and 1
     for genome_id, genome in genomes:
@@ -56,40 +57,45 @@ def pit(genome1, genome2, config):
 
     board = mancala.Board(False)
 
-    while board.checkEmpty() == False:
-        print('inside while')
-        turn = board.getTurn()
-        ran = random.random()
-        print(ran < epsilon)
-        if turn == mancala.Turn.P1:
-            obs = board.P1View()
-            action = net1.activate(obs)
-            if ran > epsilon:
+    for _ in range(1000):
+        if board.checkEmpty() == False:
+            turn = board.getTurn()
+            # input()
+            ran = random.random()
+            if turn == mancala.Turn.P1:
+                obs = board.P1View()
+                # print('p1',obs)
+                action = net1.activate(obs)
+                # if ran > epsilon:
                 action = np.argmax(action)
+                # else:
+                #     print('random')
+                #     action = random.randint(0,5)
+                # print('action',action)
+                board.P1Move(int(action))
             else:
-                print('random')
-                action = random.randint(0,5)
-            print('action',action)
-            board.P1Move(int(action))
-        else:
-            obs = board.P2View()
-            action = net2.activate(obs)
-            if ran > epsilon:
+                obs = board.P2View()
+                # print('p2',obs)
+                action = net2.activate(obs)
+                # if ran > epsilon:
                 action = np.argmax(action)
-            else:
-                print('random')
-                action = random.randint(0,5)
-            print('action',action)
-            board.P2Move(int(action))
+                # else:
+                #     print('random')
+                #     action = random.randint(0,5)
+                # print('action',action)
+                board.P2Move(int(action))
+
+                # print('p1 genome test')
+                # print('[0, 4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4]', net1.activate([0, 4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4]))
 
     genome1_pts, genome2_pts = board.getScore()
     # print(str(genome1) + ' vs. ' + str(genome2) + ': ' + str(genome1_pts) + ' to ' + str(genome2_pts))
 
     print(board.P1View())
-    print('genome1_pts:',genome1_pts)
+    # print('genome1_pts:',genome1_pts)
     genome1.fitness += (genome1_pts/48)
-    print('genome2_pts:',genome2_pts)
-    genome2.fitness += (genome1_pts/48)
+    # print('genome2_pts:',genome2_pts)
+    # genome2.fitness += (genome1_pts/48)
 
 def run(config_file):
     config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
